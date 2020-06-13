6d6df07c02887625ee237056081ea82f42521c78
Vadim <svanovivan@gmail.com>
2020-06-13 17:57:35 +0300

    Updates

---
+++ precache-manifest.88ca817b810a3ed0620ec381f8cadecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9e2b4533c498a784bf4fe92032c1cf",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "fd218580ec2333f6eb88",
+    "url": "/goit-react-hw-02-feedback/static/css/main.8c05fc72.chunk.css"
+  },
+  {
+    "revision": "d000ee9c2af5c59e3289",
+    "url": "/goit-react-hw-02-feedback/static/js/2.03338d0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.03338d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd218580ec2333f6eb88",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1f50fdec.chunk.js"
+  },
+  {
+    "revision": "7da700a07c17c956736d",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.0122c205.js"
+  }
+]);
