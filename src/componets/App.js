dc9def9a20443f26ced6338bb9effe1c7c5a85e4
Vadim <svanovivan@gmail.com.com>
2020-06-15 22:38:24 +0300

    Add style

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import FeedbackOptions from './FeedBackControls/FeedbackOptions';
-import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
 
 class App extends Component {
   static propTypes = {};
